e20dae502c2d1fa3718fdcfe854c3b304f4af3d0
Parvin Ghosh <33565397+parvin1991@users.noreply.github.com>
2021-03-21 20:26:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 31000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Node js application deployed successfully')
+  response.send('Hello Node js application deployed successfully')
 }) 
 
 app.listen(app.get('port'), function() {
